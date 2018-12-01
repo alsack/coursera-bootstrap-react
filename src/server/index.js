6eb5ab1b52c4f75b410136cc7050da81fff278e4
alsack <sacko87@gmail.com>
2018-12-01 14:20:24 +0000

    added eslint linting to enforce code styling

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 const router = require('./web/router.js');
 
-//launch server
-const node_web_server = require('node_web_server/src/index.js');
+// launch server
+const nodeWebServer = require('node_web_server/src/index.js');
 
-//add routes
-node_web_server.app.use('/', router);
+// add routes
+nodeWebServer.app.use('/', router);
 
-module.exports = { node_web_server };
+module.exports = {nodeWebServer};
--- src/server/web/router.js
+++ src/server/web/router.js
@@ -4,19 +4,20 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, '../../client/public');
 const distPath = path.resolve(__dirname, '../../client/dist');
 
-var router = express.Router();
+const router = new express.Router();
 
-//serve file from public/dist folders
+// serve file from public/dist folders
 router.use('/', express.static(publicPath));
 router.use('/', express.static(distPath));
 
-//serve files from node_modules
-const bootstrapCssDir = path.resolve(__dirname, '../../../node_modules/bootstrap/dist/css');
+// serve files from node_modules
+const bootstrapCssDir = path.resolve(__dirname,
+    '../../../node_modules/bootstrap/dist/css');
 router.use('/css', express.static(bootstrapCssDir));
 
-//If nothing else handles the request, send a 404 not found
+// If nothing else handles the request, send a 404 not found
 router.use('/', (req, res) => {
-    res.send(404);
-})
+  res.send(404);
+});
 
 module.exports = router;
