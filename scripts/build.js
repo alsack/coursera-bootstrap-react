fb5f33d9a12b5b4f48a9d9f8b181ffd3ea6aee36
alsack <sacko87@gmail.com>
2018-12-01 14:53:06 +0000

    added linting as part of build script

--- scripts/build.js
+++ scripts/build.js
@@ -4,13 +4,12 @@ const webpackConfig = require('../webpack.config');
 const compiler = webpack(webpackConfig);
 
 compiler.run((err, stats) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    console.log(stats.toString({
-        chunks: false,  // Makes the build much quieter
-        colors: true    // Shows colors in the console
-    }));
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stats.toString({
+    chunks: false, // Makes the build much quieter
+    colors: true // Shows colors in the console
+  }));
 });
