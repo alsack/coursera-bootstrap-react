8ec66363b9e25b9be699967548a11a3c5b3a07be
alsack <sacko87@gmail.com>
2018-12-01 15:19:23 +0000

    fixed about-us references

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,38 @@
 const path = require('path');
 
-//Define the webpack config object
-var bundles =[{
+// Define the webpack config object
+let bundles = [{
   mode: 'development',
   entry: path.resolve(__dirname, 'src/client/lib/index.js'),
   output: {
     path: path.resolve(__dirname, 'src/client/dist'),
     filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+  }
+}, {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/client/lib/aboutus/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'src/client/dist'),
+    filename: 'aboutus.js'
   }
 }];
+
+bundles = bundles.map((b) => {
+  return Object.assign(b, {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    }
+  });
+});
 
 module.exports = bundles;
