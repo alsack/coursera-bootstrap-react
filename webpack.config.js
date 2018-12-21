2350b52058943a04ab23a68965a4b07b31911b1c
alsack <sacko87@gmail.com>
2018-12-21 13:40:29 -0500

    dev/deploy environment updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,61 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // Define the webpack config object
-let bundles = [{
+const bundle = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/client/lib/index.js'),
+  entry: {
+    bundle: path.resolve(__dirname, 'src/client/lib/index.js'),
+    globalLoader: path.resolve(__dirname, 'src/client/lib/util/globalLoader.js')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['src/client/dist'])
+  ],
   output: {
     path: path.resolve(__dirname, 'src/client/dist'),
-    filename: 'bundle.js'
-  }
-}, {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/client/lib/util/globalLoader.js'),
-  output: {
-    path: path.resolve(__dirname, 'src/client/dist'),
-    filename: 'globalLoader.js'
-  }
-}];
-
-bundles = bundles.map((b) => {
-  return Object.assign(b, {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        }, {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }]
-        }, {
-          test: /\.(html)$/,
-          use: {
-            loader: 'html-loader',
-            options: {
-              attrs: [':data-src']
-            }
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: './.eslintrc.json'
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }, {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src']
           }
         }
-      ]
-    },
-    devtool: 'inline-source-map'
-  });
-});
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+};
 
-module.exports = bundles;
+module.exports = bundle;
