73ebb59170d3d3b074c85de5a1bd441d4d60efe7
alsack <sacko87@gmail.com>
2018-12-21 12:44:35 -0500

    Assignment 4

--- src/client/lib/loginModal.js
+++ src/client/lib/loginModal.js
@@ -2,17 +2,18 @@ import $ from 'jquery';
 import loginModal from './loginModal.html';
 
 /**
- * Appends the Modal Dialog to the html body and adds the login
- * button to the nav bar
+ * Appends the Modal Dialog to the html body and adds the login button to the nav bar
  * @param {Object} container the DOM element in which to add the login button
  */
 function addLoginModal(container) {
-  $(document.body).append(loginModal);
-  $(container).append(`
+  const modal = $(document.body).append(loginModal);
+  const loginBtn = $(container).append(`
     <span class="navbar-text">
-        <a data-toggle="modal" data-target="#loginModal">
-        <span class="fa fa-sign-in"></span> Login</a>
+        <a><span class="fa fa-sign-in"></span> Login</a>
     </span>`);
+  loginBtn.click(() => {
+    modal.find('#loginModal').modal('show');
+  });
 }
 
 export default addLoginModal;
--- src/client/lib/reserveModal.js
+++ src/client/lib/reserveModal.js
@@ -2,13 +2,17 @@ import $ from 'jquery';
 import modalHtml from './reserveModal.html';
 /**
  * Adds a modal form to reserve a table.
- * @param {Object} container the DOM element into which to add the
- * reserve button.
+ * @param {Object} container the DOM element into which to add the reserve button.
  */
 export default function addReserveModal(container) {
-  $(document.body).append(modalHtml);
+  const modal = $(document.body).append(modalHtml);
 
-  $(container).append(`
-    <a role="button" class="btn btn-lg btn-block nav-link btn-warning flex-center"
-    data-toggle="modal" data-target="#reserveModal">Reserve Table</a>`);
+  const btn = $(container).append(`
+    <a role="button" class="btn btn-lg btn-block nav-link btn-warning flex-center">
+      Reserve Table
+    </a>`);
+
+  btn.click(() => {
+    modal.find('#reserveModal').modal('show');
+  });
 };
