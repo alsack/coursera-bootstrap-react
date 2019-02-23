e95b80d4ff1f63bc74b52870b5a8b3283f8e978b
alsack <sacko87@gmail.com>
2019-02-23 08:21:33 -0500

    mods to work with new node_web_server

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,5 @@
-const createServer = require('node_web_server');
-const path = require('path');
-const fs = require('fs');
+const nodeWebServer = require('node_web_server');
 const router = require('./web/router.js');
-
-const rootDir = path.resolve(__dirname, '..');
-
-// load process.env variables from .env file
-require('dotenv').config();
-
-const port = process.env.PORT;
-let sslKeys = undefined;
-
-if (process.env.HTTPS === 'true') {
-  sslKeys = {
-    cert: fs.readFileSync(path.join(rootDir, process.env.CERT)),
-    key: fs.readFileSync(path.join(rootDir, process.env.KEY))
-  };
-}
-
-// create server.
-const nodeWebServer = createServer(port, sslKeys);
 
 // add routes
 nodeWebServer.app.use('/', router);
